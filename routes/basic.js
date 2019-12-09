12428d12039ed9e1e7c9976e918fde4d5ee99379
Ivan Abadzhiev <abadzhievltd@gmail.com>
2019-12-09 08:46:32 +0200

    Added cache to the weather request

--- routes/basic.js
+++ routes/basic.js
@@ -1,4 +1,6 @@
 const request = require("superagent");
+const NodeCache = require( "node-cache" );
+const cache = new NodeCache();
 
 module.exports = ({ router }) => {
     
@@ -10,10 +12,20 @@ module.exports = ({ router }) => {
 
         const { API_BASE_URL, OPEN_WEATHER_APP_ID } = process.env;
         const { city } = ctx.params;
+
+        // If exists in the cache get it from there
+        const response = cache.get( "weather-sofia" );
+
+        if(response) {
+            ctx.body = response;
+            return response;
+        }
         
         await request
             .get(`${API_BASE_URL}?q=${city}&appid=${OPEN_WEATHER_APP_ID}&units=metric`)
             .then(res => {
+                //set the response in the cache
+                cache.set( "weather-sofia", res.body, 6000 );
                 ctx.body = res.body;
             })
             .catch(err => {
