4fe778d87a7900192ee37237e18e9e9ee7a0d318
Ivan Abadzhiev <abadzhievltd@gmail.com>
2019-12-09 08:47:11 +0200

    Fixed lint errors

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,40 +1,40 @@
-const request = require('supertest');
+const request = require("supertest");
 
-const server = require('../server.js');
+const server = require("../server.js");
 
 // close the server after each test
 afterAll(() => {
  server.close();
- console.log('server closed!');
+ console.log("server closed!");
 });
 
-describe('basic route tests', () => {
+describe("basic route tests", () => {
 
     let response;
 
     beforeEach(async () => {
-        response = await request(server).get('/');
+        response = await request(server).get("/");
     });
  
-    test('get home route GET /', async () => {
+    test("get home route GET /", async () => {
         expect(response.status).toEqual(200);
         
     });
 
-    test("Home page text should be 'Weather API v0.1'", () => {
-        expect(response.text).toContain('Weather API v0.1');
+    test("Home page text should be "Weather API v0.1"", () => {
+        expect(response.text).toContain("Weather API v0.1");
     });
 
 });
 
-describe('weather test', () => {
+describe("weather test", () => {
     let response;
     
     beforeEach(async () => {
-        response = await request(server).get('/weather/sofia');
+        response = await request(server).get("/weather/sofia");
     });
 
-    test('get weather for sofia should return 200 ok', async () => {
+    test("get weather for sofia should return 200 ok", async () => {
       expect(response.status).toEqual(200);
     });
 });
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const Koa = require("koa");
 const Router = require("koa-router");
-const logger = require('koa-logger');
-const cors = require('@koa/cors');
+const logger = require("koa-logger");
+const cors = require("@koa/cors");
 const dotenv = require("dotenv");
 
 const app = new Koa();
 
 const router = new Router();
-require('./routes/basic')({ router });
+require("./routes/basic")({ router });
 
 // Configuration to get the env variables
 dotenv.config();
@@ -19,12 +19,12 @@ app.use(async (ctx, next) => {
     } catch (err) {
       ctx.status = err.status || 500;
       ctx.body = err.message;
-      ctx.app.emit('error', err, ctx);
+      ctx.app.emit("error", err, ctx);
     }
 });
 
 // require our external routes and pass in the router
-require('./routes/basic')({ router });
+require("./routes/basic")({ router });
 
 // tells the router to use all the routes that are on the object
 app.use(router.routes());
